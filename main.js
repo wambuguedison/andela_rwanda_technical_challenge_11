2f9172aa137ef7e9ac30a1a9226a1a00938e5810
Edison Waweru <wambuguedison@gmail.com>
2019-08-19 17:26:32 +0300

    Fetch working correctly

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 "use strict";
 const ul = document.getElementById("results");
-const url = "http://tiny.cc/fdj6az";
+const url =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-fetch(url)
-  .then(resp => resp.json())
-  .then(function(data) {
-    if (!response.ok) {
-      alert("Api Error");
+fetch(url, { mode: "cors" })
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log(
+        "Looks like there was a problem. Status Code : " + response.status
+      );
+      return;
     }
-    //continue here
+    response.json().then(function(data) {
+      console.log(data);
+    });
   })
   .catch(function(error) {
-    alert("Something went wrong" + error);
+    console.log("fetch error :-S", error);
   });
