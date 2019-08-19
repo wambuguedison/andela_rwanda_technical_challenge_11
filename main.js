96d1a5b3e1a6e8d26b8215047c2f0bdb2f9b1d77
Edison Waweru <wambuguedison@gmail.com>
2019-08-19 17:28:26 +0300

    createNode and append functions created

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 "use strict";
+const createNode = element => {
+  return document.createElement(element);
+};
+const append = (parent, element) => {
+  return parent.appendChild(element);
+};
 const ul = document.getElementById("results");
 const url =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
