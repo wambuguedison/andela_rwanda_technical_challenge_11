df9dcac5ea5dc6b284894bc7561f0fffd4fdf10b
Eddie Waweru <wambuguedison@gmail.com>
2019-11-24 00:38:53 +0300

    Basic Structure for data and coresponding titles

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ const append = (parent, element) => {
   return parent.appendChild(element);
 };
 const ul = document.getElementById("results");
-const url =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+//const url =
+  //"https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-fetch(url, { mode: "cors" })
+const url = "http://localhost/cities/cities.json";
+
+fetch(url, { mode: "no-cors" })
   .then(function(response) {
     if (response.status !== 200) {
       console.log(
@@ -21,27 +23,38 @@ fetch(url, { mode: "cors" })
       let cities = data;
       console.log(data[999]);
       return cities.map(town => {
-        let li = createNode("li"),
+        let dataDiv = createNode("div"),
+          li = createNode("li"),
           pName = createNode("p"),
-          pPoplation = createNode("p"),
+          pPopulation = createNode("p"),
           pState = createNode("p"),
-          pGrowth = createNode("p");
-        pName.innerHTML = town["city"];
-        pPoplation.innerHTML = town["population"];
-        pGrowth.innerHTML = town["growth_from_2000_to_2013"];
-        pState.innerHTML = town["state"];
-        append(li, pName);
-        append(li, pPoplation);
-        append(li, pGrowth);
-        append(li, pState);
+          pGrowth = createNode("p"),
+          spanGrowth = createNode("span");
+
+        pName.innerHTML = "City : " + "<span>" + town["city"] + "</span>";
+        pPopulation.innerHTML = "Population : " + "<span>" + town["population"] + "</span>";
+        pGrowth.innerHTML = "Population Growth : ";
+        spanGrowth.innerHTML = town["growth_from_2000_to_2013"];
+        pState.innerHTML = "State : " + "<span>" + town["state"] + "</span>";
+
+        append(dataDiv, pName);
+        append(dataDiv, pPopulation);
+        append(dataDiv, pGrowth);
+        append(pGrowth, spanGrowth);
+        append(dataDiv, pState);
+        append(li, dataDiv);
+
         append(ul, li);
+
+
+        //check if growth is lower or greater than zero and color it accordingly
         let growth = town["growth_from_2000_to_2013"];
         growth = Number(growth.replace("%", ""));
         if (growth < 0) {
-          pGrowth.style.color = "red";
+          spanGrowth.style.color = "red";
         }
         if (growth > 0) {
-          pGrowth.style.color = "green";
+          spanGrowth.style.color = "green";
         }
       });
     });
